4ac8983bf2109d449067191cd14ab27bafd00ab5
İbrahim Dönmez <152610159+ibrahimdonmez123@users.noreply.github.com>
2024-08-27 21:26:11 +0300

    27August 2024

--- TaskKifyPro.WebAPI/Program.cs
+++ TaskKifyPro.WebAPI/Program.cs
@@ -1,15 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using TaskKifyPro.Business.Abstract;
+using TaskKifyPro.Business.Services;
+using TaskKifyPro.DataAccess.Abstract;
+using TaskKifyPro.DataAccess.Context;
+using TaskKifyPro.DataAccess.EntityFramework;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TaskifyProDbContext>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITeamService, TeamService>();
+builder.Services.AddScoped<IUserDutyService, UserDutyService>();
+builder.Services.AddScoped<IDutyService, DutyService>();
+builder.Services.AddScoped<IPerformanceService, PerformanceService>();
+builder.Services.AddScoped<INotificationService, NotificationService>();
+
+
+builder.Services.AddScoped<IUserDal, UserDal>();
+builder.Services.AddScoped<ITeamDal, TeamDal>();
+builder.Services.AddScoped<IUserDutyDal, UserDutyDal>();
+builder.Services.AddScoped<IDutyDal, DutyDal>();
+builder.Services.AddScoped<IPerformanceDal, PerformanceDal>();
+builder.Services.AddScoped<INotificationDal, NotificationDal>();
+
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,7 +50,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(builder => builder
+    .WithOrigins("http://localhost:3000")
+    .WithHeaders("Authorization")
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+);
 app.UseAuthorization();
 
 app.MapControllers();
